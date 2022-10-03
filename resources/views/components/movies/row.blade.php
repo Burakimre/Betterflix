82f94b78ebc66c71987aba2427242a92a543f63d
Burak Imre <burakimre1999@gmail.com>
2022-10-03 12:22:08 +0200

    fixed movie row items height not being consistent

--- resources/views/components/movies/row.blade.php
+++ resources/views/components/movies/row.blade.php
@@ -10,31 +10,12 @@
                 @foreach (json_decode($media) as $item)
                     <div class="flex-none w-1/2 md:w-1/4 lg:w-1/5 xl:w-1/6 px-[0.2vw] first:pl-0">
                         <a href="#">
-                            <div class="relative overflow-hidden rounded-[0.2vw]">
-                                <img class="w-full" src="https://image.tmdb.org/t/p/w300/{{ $item->backdrop_path }}">
+                            <div class="h-full relative overflow-hidden rounded-[0.2vw]">
+                                <img class="h-full w-full" src="https://image.tmdb.org/t/p/original/{{ $item->poster_path }}">
                             </div>
                         </a>
                     </div>
                 @endforeach
-
-                {{-- @for ($i = 0; $i < 24; $i++)
-                    <div class="flex-none w-1/2 md:w-1/4 lg:w-1/5 xl:w-1/6 px-[0.2vw] first:pl-0">
-                        <a href="#">
-                            <div class="relative overflow-hidden rounded-[0.2vw]">
-                                <img class="w-full" src="https://image.tmdb.org/t/p/w300/{{ $data->backdrop_path }}">
-                            </div>
-                            @if ($i % 2 == 0)
-                                <div class="relative overflow-hidden rounded-[0.2vw]">
-                                    <img class="w-full" src="{{ url('/images/sandman.jpg') }}">
-                                </div>
-                            @else
-                                <div class="relative overflow-hidden rounded-[0.2vw]">
-                                    <img class="w-full" src="{{ url('/images/jojo.jpg') }}">
-                                </div>
-                            @endif
-                        </a>
-                    </div>
-                @endfor --}}
             </div>
             <span class="flex justify-center absolute top-0 right-0 bottom-0 w-[4%] bg-neutral-900/50 hover:bg-neutral-900/[70%] text-white rounded-l-[4px] cursor-pointer [&>*]:hover:scale-125">
                 <b class="hidden group-hover:flex font-['nf-icon'] font-normal text-[2.5vw] self-center transition ease-out delay-[0ms] hover:font-bold before:content-['\e867']"></b>
